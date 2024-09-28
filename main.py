612824814ea3d37987a3c78d113f8cedd315b54f
andreikin <andreikin@mail.ru>
2024-09-28 16:12:07 +0300

    test_commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import queue
 import tempfile
@@ -8,11 +7,11 @@ from pprint import pprint
 
 from PyQt5 import QtCore, QtWidgets
 import moviepy.editor as editor
-from telepot.namedtuple import InputMediaVideo
+from telepot.namedtuple import InputMediaVideo, InputMediaPhoto
 
 from constants import SETTINGS_FILE, SENDING_TMP_FILE
 from file_list_widget import FileListWidget
-from settings import PROJECT_LIST, RENDER_BOT_ID, ARDENA_BOT_ID, TEST_MODE, BOT_ID
+from settings import PROJECT_LIST, RENDER_BOT_ID, ARDENA_BOT_ID, TEST_MODE, BOT_ID, VIDEO_FORMATS, PHOTO_FORMATS
 from window import Ui_MainWindow
 
 import telepot
@@ -25,6 +24,7 @@ BOT_TOKEN = env('BOT_TOKEN')  # Получаем и сохраняем знач�
 if TEST_MODE:
     ARDENA_BOT_ID = BOT_ID
     RENDER_BOT_ID = BOT_ID
+
 
 class BaseThread(QtCore.QThread):
     def __init__(self, queue, parent=None):
@@ -43,6 +43,7 @@ class ThreadQueue:
     """
     Creates a separate thread with a queue in which functions are dropped
     """
+
     def __init__(self):
         self.queue = queue.Queue()  # Create a queue
         self.threads = []
@@ -67,17 +68,11 @@ class Messanger(ThreadQueue):
         self.ui.project_combo_box.addItems(PROJECT_LIST)
         self.ui.project_combo_box.currentIndexChanged.connect(self.switch_project)
         self.ui.path_button.clicked.connect(self.add_path)
-
         file_path = os.path.join(tempfile.gettempdir(), SETTINGS_FILE)
         self.settings = QtCore.QSettings(file_path, QtCore.QSettings.IniFormat)
-
         self.ui.file_list_widget = FileListWidget([])
-
         self.ui.path_layout.insertWidget(0, self.ui.file_list_widget)
-
         self.ui.progressBar.hide()
-
-
         self.window.show()
         self.load_settings()
 
@@ -89,16 +84,13 @@ class Messanger(ThreadQueue):
         file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self.window, "Select file", saved_dir,
                                                              "MP4 Video Files (*.mp4)", None,
                                                              QtWidgets.QFileDialog.DontConfirmOverwrite)
-
         if not file_path:
             return
-
         self.ui.file_list_widget.create_item(file_path, file_path)
         self.settings.setValue('video_folder', file_path)
 
-
     def clear_fields(self):
-        self.ui.path_line_edit.clear()
+        self.ui.file_list_widget.clear()
         self.ui.name_line_edit.clear()
         self.ui.message_text_edit.clear()
 
@@ -106,63 +98,92 @@ class Messanger(ThreadQueue):
         text = self.ui.message_text_edit.toPlainText()
         text = text + "\n" if text else ""
         asset = self.ui.name_line_edit.text()
-
-
         data = {'file_path': self.ui.file_list_widget.get_list()}
 
-
-        if not asset or not data['file_path']:
-            print("The asset or file name being sent is not specified.")
-            self.ui.name_line_edit.setPlaceholderText('Enter the asset name here')
-        #     self.ui.path_line_edit.setPlaceholderText('Specify the path to the video file')
-            return
+        # if not asset or not data['file_path']:
+        #     print("The asset or file name being sent is not specified.")
+        #     return
 
         data['project'] = self.ui.project_combo_box.currentText()
         wareframe = '#wareframe' if self.ui.wareframe_check_box.isChecked() else ""
         data['message'] = f'{text}{data["project"]} #{asset} {wareframe}'
-
-        pprint(data)
-
         return data
 
     def run_thread(self):
-        data = {}
         # data = self.get_data()
-        # if not data:
-        #     return
+        data = {'file_path': ['C:/Users/Andrew/Desktop/Clerk_Girl_Elvis_Skin.mp4',
+                              'C:/Users/Andrew/Desktop/Tzaangor_shaman.mp4',
+                              'C:/Users/Andrew/Desktop/Screenshot_2.jpg'],
+                'project': '#bvr', 'message': '#bvr #Asset #wareframe'}
+
+        if not data:
+            return
         self.add_task(lambda: self.send_message(data))
-        # self.clear_fields()
+        self.clear_fields()
 
     def send_message(self, data):
         try:
-            # file_path = data['file_path']
-            # if self.is_file_larger_than_45mb(file_path):
-            #     new_name = os.path.join(tempfile.gettempdir(), SENDING_TMP_FILE)
-            #     clip = editor.VideoFileClip(file_path)
-            #     clip.write_videofile(new_name)
-            #     file_path = new_name
+            print(data)
 
-            bot = telepot.Bot(BOT_TOKEN)
-            file_path = 'C:/Users/avbeliaev/Desktop/video_2024-06-27_09-41-42.mp4'
+            media_group = []
+            for i, each in enumerate(data['file_path']):
+                text = data['message'] if data['message'] and i == 0 else ''
 
-            # medias = [types.InputMediaPhoto('https://habrastorage.org/webt/61/99/de/6199de8312758776787774.png'),types.InputMediaPhoto(
-            #               'https://habrastorage.org/getpro/habr/upload_files/1fd/40a/c44/1fd40ac441a0aee55fc90e67997437af.png')]
+                # if Path(each).suffix in VIDEO_FORMATS:
+                #     clip = editor.VideoFileClip(each)
+                #     width, height = clip.size
+                #
+                #     video = InputMediaVideo(media=open(each, 'rb'), caption=text, width=width, height=height,
+                #                             mimetypes='video/mp4')
+                #     media_group.append(video)
+                #
+                # if Path(each).suffix in PHOTO_FORMATS:
+                #     video = InputMediaPhoto(media=open(each, 'rb'), caption=text)
+                #     media_group.append(video)
+                #
+                # print(Path(each).suffix)
 
-            f_media = InputMediaVideo()
 
-
-
-            print (f_media)
-            #bot.sendMediaGroup(RENDER_BOT_ID, media=[f_media, f_media])
-
-
-            # bot = telepot.Bot(BOT_TOKEN)
-            # with open(file_path, 'rb') as f:
-            #     bot.sendVideo(RENDER_BOT_ID, f, caption=data['message'])
+            # video = InputMediaVideo(media=open(data['file_path'][0], 'rb'), caption=text, width=400,
+            #                         thumb=open(data['file_path'][2], 'rb'), height=800, mimetypes='video/mp4')
             #
-            # if data['project'] == PROJECT_LIST[0]:
-            #     with open(file_path, 'rb') as f:
-            #         bot.sendVideo(ARDENA_BOT_ID, f, caption=data['message'])
+            # media_group.append(video)
+            #
+            bot = telepot.Bot(BOT_TOKEN)
+            # bot.sendMediaGroup(BOT_ID, media=media_group)
+
+            video_file = open(data['file_path'][0], 'rb')
+            thumbnail_file = open(data['file_path'][2], 'rb')
+
+            video = InputMediaVideo(media=video_file, caption=text, thumb=thumbnail_file, mimetypes='video/mp4')
+
+            bot.sendMediaGroup(BOT_ID, media=[video])
+
+            #bot.sendVideo(RENDER_BOT_ID, video=video_file, caption=data['message'], thumb=thumbnail_file)
+
+            video_file.close()
+            thumbnail_file.close()
+
+
+
+
+                # file_path = Path("example.txt")
+                # extension = file_path.suffix
+
+        # file_path = data['file_path']
+        # if self.is_file_larger_than_45mb(file_path):
+        #     new_name = os.path.join(tempfile.gettempdir(), SENDING_TMP_FILE)
+        #     clip = editor.VideoFileClip(file_path)
+        #     clip.write_videofile(new_name)
+        #     file_path = new_name
+
+        # bot = telepot.Bot(BOT_TOKEN)
+        # with open(file_path, 'rb') as f:
+        #     bot.sendVideo(RENDER_BOT_ID, f, caption=data['message'])
+        #
+        # if data['project'] == PROJECT_LIST[0]:
+        #     with open(file_path, 'rb') as f:
+        #         bot.sendVideo(ARDENA_BOT_ID, f, caption=data['message'])
 
         except Exception as message:
             print(message)
@@ -172,7 +193,6 @@ class Messanger(ThreadQueue):
         file_size = os.path.getsize(file_path)
         size_in_bytes = 45 * 1024 * 1024  # 45 МБ в байтах
         return file_size > size_in_bytes
-
 
     def load_settings(self):
         """
@@ -190,6 +210,7 @@ class Messanger(ThreadQueue):
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     window = Messanger()  # Создаем экземпляр класса
     sys.exit(app.exec_())  # Запускаем цикл обработки событий
--- settings.py
+++ settings.py
@@ -4,4 +4,10 @@ PROJECT_LIST = "#ardena", "#MGS", "#alaska", "#amber", "#wwz", "#bvr", "#thunder
 BOT_ID = 476369950
 ARDENA_BOT_ID = -1001948513915
 RENDER_BOT_ID = -1001719029113
+
+VIDEO_FORMATS = ['.mp4']
+PHOTO_FORMATS = ['.png', '.jpg']
+
 TEST_MODE = True
+
+SETTINGS_SETTING = True
